73c3be9c096f20b6c01d15e963b538dc999c0722
Natali2707 <105042840+Natali2707@users.noreply.github.com>
2022-10-14 12:27:53 +0300

    education

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,30 +1,58 @@
-// import './styles/header.css';
-import style from './styles/avatar.module.css';
-// import logo from './images/avatar.png'
+import style from "./styles/avatar.module.css";
+import ava from './images/avatar.png';
+import teach from './images/logo-teach.png';
+import skillUp from './images/skillUp.png';
 
-const Avatar=()=>{
-    return(
-        <div className={style.head}>
-            <nav className={style.navigation}>
-           <div className={style.avatar}> 
-           
-           <img 
-        // style={{display:'none'}}
-        className={style.pic}
-        alt={style.pic}
-        src={process.env.PUBLIC_URL+'avatar.png'}/>
-        <img 
-        />
-           </div>
-           <div className={style.education}>
-            <ul>
-                <li className={style.educ}>Mogilev University</li>
-                <li className={style.educ}>Mogilev University</li>
-                <li className={style.educ}>Mogilev University</li>
-            </ul>
-           </div>
-            </nav>
+const Avatar = () => {
+  return (
+    <div className={style.head}>
+      <nav className={style.navigation}>
+        <div className={style.avatar}>
+        <img  className={style.pic} src={ava}  alt=" myPhoto" />
+          {/* <img
+            className={style.pic}
+            alt="my photo"
+            src={process.env.PUBLIC_URL + "avatar.png"}
+          /> */}
         </div>
-    )
-}
-export default Avatar; 
+
+
+        <div className={style.introduce}>
+            <h1>ALISEVICH NATALLIA</h1>
+            <h3>OBJECTIVE:<strong>
+            Front End Developer</strong></h3>
+        </div>
+        
+        <div className={style.education}>
+          <h1>Education and Training</h1>
+          <ul>
+          <li className={style.educ}>  
+          TeachMeSkills <img  className={style.logo} src={teach}  alt=" TeachMeSkills" />April 2022-...
+          <h4>Frontend/React.js Developer</h4>
+             </li>
+          <li className={style.educ}>
+          SkillUp <img  className={style.logo} src={skillUp}  alt="SkillUp" />June 2021-October 2021 
+          <h4> QA-engineer. Writing test documentation: bug reports (Jira), test plan,check-lists (Jira, TestRail), test cases (Jira, TestRail)</h4>
+             </li>
+            <li className={style.educ}>
+              Mogilev Technological Institute
+              <h4>Accountant-economist</h4>
+            </li>
+           
+            <li className={style.educ}>
+              Mogilev Technological Institute
+              <h4>Process Engineer and Chemical Engineer <br/>
+              <i>
+                Winner of the republican competition of scientific works among
+                university students
+              </i></h4>
+            </li>
+          </ul>
+        </div>
+
+
+      </nav>
+    </div>
+  );
+};
+export default Avatar;
--- src/components/PersonInfo.js
+++ src/components/PersonInfo.js
@@ -1,13 +1,62 @@
-import styles from './styles/personInfo.module.css';
+import styles from "./styles/personInfo.module.css";
 
-const PersonInfo =()=>{
-    return(
-        <ul className={styles.personInfo}>
-            <li>github</li>
-            <li>linkedin</li>
-            <li>email</li>
-        </ul>
-       
-    )
-}
+const PersonInfo = () => {
+  return (
+    <div className={styles.personInfo}>
+      <div className={styles.address}>
+        <img
+          className={styles.icon}
+          alt="address"
+          src={process.env.PUBLIC_URL + "house.jpg"}
+        />
+        Belarus, Minsk
+      </div>
+      <div className={styles.icon}>
+        <img
+          className={styles.icon}
+          alt="phone"
+          src={process.env.PUBLIC_URL + "house.jpg"}
+        />
+        github
+      </div>
+      <div className={styles.icon}>
+        <img
+          className={styles.icon}
+          alt="phone"
+          src={process.env.PUBLIC_URL + "house.jpg"}
+        />
+        HTTPS://WWW.LINKEDIN.COM/IN/NATALLIA- ALISEVICH/
+      </div>
+      <div className={styles.icon}>
+        {" "}
+        <img
+          className={styles.icon}
+          alt="house"
+          src={process.env.PUBLIC_URL + "house.jpg"}
+        />
+        ALISEVICH.NATALI@GMAIL.COM
+      </div>
+      <div className={styles.icon}>
+        {" "}
+        <img
+          className={styles.icon}
+          alt="phone"
+          src={process.env.PUBLIC_URL + "house.jpg"}
+        />
+        +375 29 746 54 18
+      </div>
+      <div>
+        I am a Frontend/React.js Developer with a technical background.
+        {/* I already have experience with
+functional, integration, system,
+and user acceptance testing
+during my software testing
+training. */}
+        I can participate in creating useful beautiful products and solving
+        business problems.I intend to grow professionally and improve my skills
+        Now I'm eager to join project where I can use my skills.
+      </div>
+    </div>
+  );
+};
 export default PersonInfo;
